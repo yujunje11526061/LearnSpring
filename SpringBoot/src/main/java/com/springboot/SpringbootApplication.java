9135036cca7ca1fe4ddd73994a2309017e7db139
Junjie_Yu <yujunjie@zju.edu.cn>
2019-05-22 23:54:12 +0800

    SpringBoot 初始化

--- SpringBoot/src/main/java/com/springboot/SpringbootApplication.java
+++ SpringBoot/src/main/java/com/springboot/SpringbootApplication.java
@@ -1,10 +1,18 @@
 package com.springboot;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.ComponentScans;
 
+
+
+
+
+//此为引导类，也是主配置类
+
+//@EnableAutoConfiguration() 此注解用于排除不需要自动配置的类。
 //默认情况下spring boot只会扫描启动类当前包和以下的包，别的包需要用String[]指定
 @ComponentScan(basePackages = {"com"})
 @SpringBootApplication
